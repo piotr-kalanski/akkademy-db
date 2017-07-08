b91ae13f72bb9b546f651cd73c89c2e6c5b01834
kalan <piotr.kalanski@gmail.com>
2017-07-08 13:09:53 +0200

    SetRequest

---
+++ src/main/scala/com/akkademy/AkkademyDb.scala
@@ -0,0 +1,19 @@
+package com.akkademy
+
+import akka.actor.Actor
+import akka.event.Logging
+import com.akkademy.messages.SetRequest
+
+import scala.collection.mutable
+
+class AkkademyDb extends Actor {
+  val map = new mutable.HashMap[String, Object]()
+  val log = Logging(context.system, this)
+  override def receive: Receive = {
+    case SetRequest(key, value) => {
+      log.info("received SetRequest - key: {} value: {}", key, value)
+      map.put(key, value)
+    }
+    case o => log.info("received unknow message: {}", o)
+  }
+}
---
+++ src/test/scala/com/akkademy/AkkademyDbSpec.scala
@@ -0,0 +1,21 @@
+package com.akkademy
+
+import akka.actor.ActorSystem
+import akka.testkit.TestActorRef
+import com.akkademy.messages.SetRequest
+import org.scalatest.{BeforeAndAfterEach, FunSpecLike, Matchers}
+
+class AkkademyDbSpec extends FunSpecLike with Matchers with BeforeAndAfterEach {
+  implicit val system = ActorSystem()
+
+  describe("akkademy") {
+    describe("given SetRequet") {
+      it("should place key/value into map") {
+        val actorRef = TestActorRef(new AkkademyDb)
+        actorRef ! SetRequest("key", "value")
+        val akkademyDb = actorRef.underlyingActor
+        akkademyDb.map.get("key") should equal(Some("value"))
+      }
+    }
+  }
+}
